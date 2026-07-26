e310826a5cddf633160ca9c4d246d11327d401a2
swisssweetapt <info@swisssweet-apartment.com>
2026-07-26 15:54:07 +0200

    Add files via upload

--- price-config.js
+++ price-config.js
@@ -3,7 +3,7 @@ window.PRICE_CONFIG = {
   lowSeasonPrice: 80,
   specialOffer: {
     dates: ['2026-07-28', '2026-07-29'],
-    price: 100,
-    label: '28–29 July'
+    price: 90,
+    label: '28–30 (2 nights)'
   }
 };
