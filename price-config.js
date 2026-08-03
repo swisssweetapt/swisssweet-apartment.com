3b7436692f68e8ac5d0f0721d30c279c83a33585
swisssweetapt <info@swisssweet-apartment.com>
2026-08-04 00:56:07 +0200

    Add files via upload

--- price-config.js
+++ price-config.js
@@ -1,6 +1,11 @@
 window.PRICE_CONFIG = {
-  basePrice: 110,
-  lowSeasonPrice: 80,
+  basePrice: 110,               // Août 2026
+  fallSeason: {
+    start: '2026-09-01',
+    end: '2026-11-30',
+    price: 90                   // Automne 2026 (sept-nov)
+  },
+  onDemandFrom: '2026-12-01',   // Déc 2026 et toute l'année 2027 : prix sur demande
   specialOffer: {
     dates: ['2026-08-09', '2026-08-10', '2026-08-11', '2026-08-12', '2026-08-13'],
     price: 110,
